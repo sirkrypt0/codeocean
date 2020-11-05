19abe98b6f51a0cb263646c926b63c869d3f0c6d
sirkrypt0 <22522058+sirkrypt0@users.noreply.github.com>
2020-11-05 11:25:17 +0100

    Update keys for rvm installation

--- provision.sh
+++ provision.sh
@@ -66,14 +66,14 @@ sudo docker pull openhpi/co_execenv_ruby:2.5
 
 # rvm
 sudo apt-get -qq -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
-gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | sudo bash -s stable
 
 # access rvm installation without sudo
 sudo gpasswd -a ${USER} rvm
 
 # ruby
-source /etc/profile.d/rvm.sh
+source ~/.rvm/scripts/rvm
 sg rvm "rvm install $ruby_version"
 rvm use $ruby_version --default
 sudo /usr/local/rvm/bin/rvm alias create default $ruby_version
